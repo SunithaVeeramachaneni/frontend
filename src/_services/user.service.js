6127a3f22bde15a025c5f581ac02a475a898f383
E5000285 <sunitha.veeramachaneni@innovapptive.com>
2021-02-23 16:32:40 +0530

    fixed route

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -28,7 +28,7 @@ function login(email, password) {
 
 function logout() {
     // remove user from local storage to log user out
-    localStorage.removeItem('token');
+    localStorage.removeItem('users');
 }
 
 function getAll() {
