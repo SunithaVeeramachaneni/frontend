d8827573ffe9daa0e4fcd24b4ca9755a5dfc742d
Shashank Sistla <shashank.sistla@innovapptive.com>
2021-07-26 16:31:03 +0530

    chore: code cleanup

--- src/app/apis/abap_server_dev.js
+++ src/app/apis/abap_server_dev.js
@@ -42,22 +42,37 @@ const getTime = (currentTime) => {
 }
 
 const timeout = 60000;
-const csrfTokenExpireTime = 29 // mins
+// const csrfTokenExpireTime = 29 // mins
 
 
 
-var baseUrl = 'http://innongwtst.internal.innovapptive.com:8000/sap/opu/odata/INVMWO/MWORKORDER_SRV/';      // For NGQ System
+var baseUrlWorkOrder = 'http://innongwtst.internal.innovapptive.com:8000/sap/opu/odata/INVMWO/MWORKORDER_SRV/';      // For NGQ System
+var baseUrlRace = 'http://54.208.180.228:8000/sap/opu/odata/INVCEC/RACE_SRV/'
 
-let reqHeaders = {
-  //"Authorization": "Basic " + base64.encode('MWORKINST1' + ':' + 'qwerty'),         //Credentilas for NGT System
-  // "Authorization": "Basic " + base64.encode('gurpreet.wo' + ':' + 'qa@54321'),      //Credentilas for NGQ System
-  "Authorization": "Basic " + base64.encode('mworkorder1' + ':' + 'qwerty'),         //Credentilas for NDS System
+
+
+const reqHeaders = (base) => {
+
+  let headers = {
   "Content-Type": "application/json",
   "x-csrf-token": "Fetch"
+  }
+  if (base === 'work_order')
+    headers = {"Authorization": "Basic " + base64.encode('mworkorder1' + ':' + 'qwerty'), ...headers};       //Credentilas for NDS System
+   //"Authorization": "Basic " + base64.encode('MWORKINST1' + ':' + 'qwerty'),         //Credentilas for NGT System
+  // "Authorization": "Basic " + base64.encode('gurpreet.wo' + ':' + 'qa@54321'),      //Credentilas for NGQ System
+   
+  if (base === 'race')
+    headers = {"Authorization": "Basic " + base64.encode('MWORKINST1' + ':' + 'qwerty'), ...headers};
+
+  return headers
 }
 
-const getReqHeaders = { ...reqHeaders };
-const postReqHeaders = { ...reqHeaders, 'x-csrf-token': '' };
+const getReqHeaders = (base) =>  reqHeaders(base) ;
+const postReqHeaders = (base) => { 
+  const headers = reqHeaders(base);
+  return { ...reqHeaders(base), 'x-csrf-token': '' }
+ };
 
 app.use(router);
 app.use(haltOnTimedout);
@@ -68,46 +83,12 @@ function haltOnTimedout(req, res, next) {
   if (!req.timedout) next()
 }
 
-const getCSRFToken = (res = null) => {
-  return new Promise((resolve, reject) => {
-    const url = baseUrl + 'DFormCollection?$format=json';
-    request({
-      url,
-      pool: separateReqPool,
-      headers: getReqHeaders,
-      timeout
-    }, function (error, response) {
-      if (error) {
-        postReqHeaders['x-csrf-token'] = '';
-        postReqHeaders['Cookie'] = '';
-        if (res !== null) {
-          res.status(500).json({ status: 500, message: 'Unable to set CSRF Token', error });
-        }
-        reject({ error });
-      } else {
-        postReqHeaders['x-csrf-token'] = response.headers['x-csrf-token'];
-        postReqHeaders['Cookie'] = response.headers["set-cookie"];
-        console.log(`${getTime(new Date())} : getCSRFToken : CSRF Token Set/Reset`);
-        resolve({ response });
-      }
-    })
-  })
-}
-
 
 const getErrorResponseFormat = (status, data) => {
   const { message } = data.error || {};
   const { value } = message || {};
   return { status, message: value, error: data };
 }
-
-setInterval(async () => {
-  try {
-    await getCSRFToken();
-  } catch (error) {
-    console.log(`${getTime(new Date())} : getCSRFToken : Unable to set CSRF Token : ${JSON.stringify(error)}`);
-  }
-}, 1000 * 60 * csrfTokenExpireTime)
 
 const getSelectOptions = () => {
   const selectOptionsList = ['PRIOK', 'PRIOKX', 'COLOUR', 'AUFNR', 'AUFTEXT', 'ARBPL', 'KTEXT', 'PARNR', 'WorkOrderOperationSet/STATUS']
@@ -124,13 +105,13 @@ const getSelectOptions = () => {
 }
 
 app.get('/abapapi/workOrdersAndOperations', (req, res) => {
-  let url = baseUrl + `WorkOrdersCollection?$expand=WorkOrderOperationSet&` + getSelectOptions() + '$format=json&';
+  let url = baseUrlWorkOrder + `WorkOrdersCollection?$expand=WorkOrderOperationSet&` + getSelectOptions() + '$format=json&';
 
   console.log(`${getTime(new Date())} : workOrders : ${url}`);
   request({
     url: url,
     pool: separateReqPool,
-    headers: getReqHeaders,
+    headers: (getReqHeaders('work_order')),
     timeout
   }, function (error, response) {
     if (error) {
@@ -152,13 +133,41 @@ app.get('/abapapi/workOrdersAndOperations', (req, res) => {
   });
 });
 
+app.get('/abapapi/getLogOnUserDetails', (req, res) => {
+  let url = baseUrlRace + `LogOnUserDetailsCollection` + '$format=json&';
+
+  console.log(`${getTime(new Date())} : logOnDetails : ${url}`);
+  request({
+    url: url,
+    pool: separateReqPool,
+    headers: getReqHeaders,
+    timeout
+  }, function (error, response) {
+    if (error) {
+      if (!req.timedout) {
+        res.status(500).json({ status: 500, message: 'Unable to get log on details!!', error });
+      }
+      console.log(`${getTime(new Date())} : logOnDetails : ${error}`);
+    }
+    if (!error) {
+      console.log(`${getTime(new Date())} : logOnDetails : ${response.statusCode}`);
+      if (!req.timedout && response.statusCode === 200) {
+        res.status(200).json(JSON.parse(response.body).d.results);
+      } else if (!req.timedout) {
+        res.status(response.statusCode).json(getErrorResponseFormat(response.statusCode, response.body));
+      } else {
+        console.log(`${getTime(new Date())} : logOnDetails : ServiceUnavailableError: Response timeout`);
+      }
+    }
+  });
+});
+
+
+
 app.listen(port, () => {
   console.log(`Server startred on port: ${port}`)
 }
 )
 
-app.get('/', (req, res) => {
-  console.log("Base route hit!");
-  res.json({ success: true, msg: "Base route hit!" })
-})
+
 
