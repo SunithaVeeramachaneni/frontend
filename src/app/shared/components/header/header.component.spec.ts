9a220daae5fcb2b3f61783750dd9435a94278716
Kiran Kumar Palani <kiran.palani@innovapptive.com>
2021-09-02 09:21:25 +0530

    fixed failing unit test case

--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -2,21 +2,26 @@ import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { of } from 'rxjs';
 import { CommonService } from '../../services/common.service';
+import { HeaderService } from '../../services/header.service';
+import { logonUserDetails } from '../../services/header.service.mock';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let commonServiceSpy: CommonService;
   let fixture: ComponentFixture<HeaderComponent>;
+  let headerServiceSpy: HeaderService;
 
   beforeEach(waitForAsync(() => {
     commonServiceSpy = jasmine.createSpyObj('CommonService', [], {
       minimizeSidebarAction$: of(false)
     });
+    headerServiceSpy = jasmine.createSpyObj('HeaderService', ['getLogonUserDetails']);
     TestBed.configureTestingModule({
       declarations: [ HeaderComponent ],
       providers: [
-        { provide: CommonService, useValue: commonServiceSpy }
+        { provide: CommonService, useValue: commonServiceSpy },
+        { provide: HeaderService, useValue: headerServiceSpy },
       ],
       imports:[
         IonicModule
@@ -28,6 +33,10 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    (headerServiceSpy.getLogonUserDetails as jasmine.Spy)
+      .withArgs()
+      .and.returnValue(logonUserDetails)
+      .and.callThrough();
     fixture.detectChanges();
   });
 
