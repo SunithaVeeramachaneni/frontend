1c05b69ca921e9d4a74ed45899b4ac63f3416728
Kiran Kumar Palani <kiran.palani@innovapptive.com>
2021-09-17 11:40:32 +0530

    updated environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  wiApiUrl: 'https://cwpqa.innovapptive.com/wiapi/api/',
-  wiAbapApiUrl: 'https://wi.innovapptive.com/abapapi/',
+  wiApiUrl: 'https://cwpqa.innovapptive.com/wiapi/',
+  wiAbapApiUrl: 'https://cwpqa.innovapptive.com/wiabapapi/abapapi/',
   spccAbapApiUrl: 'https://cwpqa.innovapptive.com/spccabapapi/abapapi/',
   mccAbapApiUrl: 'https://cwpqa.innovapptive.com/spccabapapi/abapapi/',
   s3BaseUrl: 'https://innovwi.s3.ap-south-1.amazonaws.com/',
