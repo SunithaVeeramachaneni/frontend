b02514213b587b60605cdba3d663025d12462295
Kiran Kumar Palani <kiran.palani@innovapptive.com>
2021-11-29 10:49:16 +0530

    updated WI title

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,7 +2,7 @@ export const routingUrls = {
   dashboard: { url: '/dashboard', title: 'Dashboard' },
   maintenance: { url: '/maintenance', title: 'Maintenance Control Center' },
   spareParts: { url: '/spare-parts', title: 'Spare Parts Control Center' },
-  workInstructions: { url: '/work-instructions', title: 'Work Instructions' },
+  workInstructions: { url: '/work-instructions', title: 'Work Instructions Authoring' },
   drafts: { url: '/work-instructions/drafts', title: 'Drafts' },
   favorites: { url: '/work-instructions/favorites', title: 'Favorites' },
   published: { url: '/work-instructions/published', title: 'Published' },
