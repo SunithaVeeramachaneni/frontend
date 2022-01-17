0458674abe076aa6f61e8453ce2c9f7af067461f
pavan.addepalli@innovapptive.com <pavan.addepalli@innovapptive.com>
2022-01-17 11:57:28 +0000

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -34,13 +34,16 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml','junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    singleRun: true,
+    restartOnFileChange: true,
+    junitReporter: {
+  outputFile: 'test-results.xml'
+     },
   });
 };
