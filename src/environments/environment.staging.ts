30f0efd00ddaaa661007fbfe3a9cc4c52d294f54
Kiran Kumar Palani <kiran.palani@innovapptive.com>
2022-03-23 14:19:20 +0530

    dockerization changes

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  dashboardApiUrl: 'https://cwpdemo.innovapptive.com/dashboardapi/',
+  mccAbapApiUrl: 'https://cwpdemo.innovapptive.com/mccspccabapapi/',
+  spccAbapApiUrl: 'https://cwpdemo.innovapptive.com/mccspccabapapi/',
+  wiApiUrl: 'https://cwpdemo.innovapptive.com/wiapi/',
+  wiAbapApiUrl: 'https://cwpdemo.innovapptive.com/wiabapapi/',
+  s3BaseUrl: 'https://innovwi.s3.ap-south-1.amazonaws.com/',
+  requestTimeout: 60000 * 3,
+  undoRedoOffset: 5
+};
