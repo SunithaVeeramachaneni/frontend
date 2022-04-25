feb941181a4aac789f6cf054db8de517754fc6ce
kavya koka <kavya.koka@innovapptive.com>
2022-04-25 20:48:12 +0530

    environment fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   spccAbapApiUrl: 'http://localhost:8003/mccspccabapapi/',
   wiApiUrl: 'http://localhost:8001/wiapi/',
   wiAbapApiUrl: 'http://localhost:8002/wiabapapi/',
-  userRoleManagementApiUrl: 'https://localhost:8007/',
+  userRoleManagementApiUrl: 'http://localhost:8007/',
   s3BaseUrl: 'https://innovwi.s3.ap-south-1.amazonaws.com/',
   requestTimeout: 60000 * 3,
   undoRedoOffset: 5
