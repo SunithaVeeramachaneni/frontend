0289036ba2b256baa632f7c577361a3a8b339305
Shiva Kanneboina <E5000557@corpinnovapptive.com>
2022-05-30 14:36:31 +0530

    updated environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  slackAPIUrl: 'http://localhost:8007/slack/',
   dashboardApiUrl: 'https://cwpdev.innovapptive.com/dashboardapi/',
   mccAbapApiUrl: 'https://cwpdev.innovapptive.com/mccspccabapapi/',
   // mccAbapApiUrl: 'http://localhost:8003/mccspccabapapi/',
@@ -8,8 +7,8 @@ export const environment = {
   spccAbapApiUrl: 'https://cwpdev.innovapptive.com/mccspccabapapi/',
   wiApiUrl: 'https://cwpdev.innovapptive.com/wiapi/',
   wiAbapApiUrl: 'https://cwpdev.innovapptive.com/wiabapapi/',
-  userRoleManagementApiUrl:
-    'https://cwpdev.innovapptive.com/userrolemanagementapi/',
+  userRoleManagementApiUrl: 'http://localhost:8007/',
+  slackAPIUrl: 'http://localhost:8007/slack/',
   s3BaseUrl: 'https://innovwi.s3.ap-south-1.amazonaws.com/',
   requestTimeout: 60000 * 3,
   undoRedoOffset: 5
