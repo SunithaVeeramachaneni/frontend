b3ab29cdb648871641fcc71fa497291961d104b6
Shiva Kanneboina <E5000557@corpinnovapptive.com>
2022-05-31 11:13:29 +0530

    review comments addressed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,25 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
   production: false,
-  dashboardApiUrl: 'https://cwpdev.innovapptive.com/dashboardapi/',
-  mccAbapApiUrl: 'https://cwpdev.innovapptive.com/mccspccabapapi/',
-  // mccAbapApiUrl: 'http://localhost:8003/mccspccabapapi/',
-  // spccAbapApiUrl: 'http://localhost:8003/mccspccabapapi/',
-  spccAbapApiUrl: 'https://cwpdev.innovapptive.com/mccspccabapapi/',
-  wiApiUrl: 'https://cwpdev.innovapptive.com/wiapi/',
-  wiAbapApiUrl: 'https://cwpdev.innovapptive.com/wiabapapi/',
+  dashboardApiUrl: 'http://localhost:8004/dashboardapi/',
+  mccAbapApiUrl: 'http://localhost:8003/mccspccabapapi/',
+  spccAbapApiUrl: 'http://localhost:8003/mccspccabapapi/',
+  wiApiUrl: 'http://localhost:8001/wiapi/',
+  wiAbapApiUrl: 'http://localhost:8002/wiabapapi/',
   userRoleManagementApiUrl: 'http://localhost:8007/',
   slackAPIUrl: 'http://localhost:8007/slack/',
   s3BaseUrl: 'https://innovwi.s3.ap-south-1.amazonaws.com/',
   requestTimeout: 60000 * 3,
   undoRedoOffset: 5
 };
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
