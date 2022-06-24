530db942f5e6ee41af361c185b3c68b16c7c91f7
Kiran Kumar Palani <kiran.palani@innovapptive.com>
2022-06-24 14:08:04 +0530

    added specs for recent implementations

--- src/app/components/user-management/permissions/permissions.component.spec.ts
+++ src/app/components/user-management/permissions/permissions.component.spec.ts
@@ -1,3 +1,4 @@
+import { Component, DebugElement, SimpleChange } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -6,18 +7,26 @@ import {
   TranslateLoader,
   TranslateModule
 } from '@ngx-translate/core';
+import { cloneDeep } from 'lodash';
 import { NgxShimmerLoadingModule } from 'ngx-shimmer-loading';
 import { of } from 'rxjs';
 import { AppMaterialModules } from 'src/app/material.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import {
+  allPermissions,
+  allPermissions$,
+  selectedRolePermissions$
+} from '../roles/roles-permissions.mock';
+import { allRolesMock } from '../services/roles-permissions.mock';
 
 import { PermissionsComponent } from './permissions.component';
-import { RolesPermissionsService } from '../services/roles-permissions.service';
 
 describe('PermissionsComponent', () => {
   let component: PermissionsComponent;
   let fixture: ComponentFixture<PermissionsComponent>;
-  let rolesPermissionsServiceSpy: RolesPermissionsService;
+  let permissionsDe: DebugElement;
+  let permissionsEl: HTMLElement;
+
   const permissionList = {
     name: 'Reports',
     checked: true,
@@ -44,31 +53,7 @@ describe('PermissionsComponent', () => {
     ]
   };
 
-  const permissions = [
-    {
-      createdAt: '2022-04-19T10:38:41.000Z',
-      displayName: 'Create Report',
-      id: 1,
-      moduleName: 'Reports',
-      name: 'CREATE_REPORT',
-      updatedAt: '2022-04-19T10:38:41.000Z'
-    },
-    {
-      createdAt: '2022-04-19T10:38:41.000Z',
-      displayName: 'Edit Report',
-      id: 2,
-      moduleName: 'Reports',
-      name: 'UPDATE_REPORT',
-      updatedAt: '2022-04-19T10:38:41.000Z'
-    }
-  ];
-
   beforeEach(async () => {
-    rolesPermissionsServiceSpy = jasmine.createSpyObj(
-      'RolesPermissionsService',
-      ['getPermissions$']
-    );
-
     await TestBed.configureTestingModule({
       declarations: [PermissionsComponent],
       imports: [
@@ -85,21 +70,29 @@ describe('PermissionsComponent', () => {
           }
         })
       ],
-      providers: [
-        {
-          provide: RolesPermissionsService,
-          useValue: rolesPermissionsServiceSpy
-        }
-      ]
+      providers: []
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PermissionsComponent);
     component = fixture.componentInstance;
-    (rolesPermissionsServiceSpy.getPermissions$ as jasmine.Spy)
-      .withArgs()
-      .and.returnValue(of([]));
+    permissionsDe = fixture.debugElement;
+    permissionsEl = permissionsDe.nativeElement;
+    component.allPermissions$ = allPermissions$;
+    component.selectedRolePermissions$ = selectedRolePermissions$;
+    component.selectedRole = allRolesMock[1];
+    component.roleFormChanged = { isChanged: true };
+    component.ngOnChanges({
+      allPermissions$: new SimpleChange(null, allPermissions$, true),
+      selectedRolePermissions$: new SimpleChange(
+        null,
+        selectedRolePermissions$,
+        true
+      ),
+      selectedRole: new SimpleChange(null, allRolesMock[1], true),
+      roleFormChanged: new SimpleChange(null, { isChanged: true }, true)
+    });
     fixture.detectChanges();
   });
 
@@ -122,28 +115,186 @@ describe('PermissionsComponent', () => {
     expect(component.fewComplete(permissionList)).toBe(false);
   });
 
-//   it('should get method updateAllChecked', () => {
-//     component.updateAllChecked(permissionList, permissionList.permissions);
-//   });
+  describe('updateAllChecked', () => {
+    beforeEach(() => {
+      spyOn(component.permissionsChange, 'emit');
+    });
 
-//   it('should get method setAllChecked', () => {
-//     const checked = false;
-//     const noPermissions = {
-//       name: 'Reports',
-//       checked: true,
-//       countOfChecked: 2,
-//       permissions: null
-//     };
+    it('should define fuction', () => {
+      expect(component.updateAllChecked).toBeDefined();
+    });
 
-//     spyOn(component.permissionsChange, 'emit');
+    it('should select `Display Users` when user select `Create User` permission (module and sub module names are same)', () => {
+      const permissions: any = cloneDeep(allPermissions);
+      permissions[0].countOfChecked = 11;
+      permissions[1].countOfChecked = 2;
+      permissions[1].permissions[0].checked = true;
+      permissions[1].permissions[1].checked = true;
 
-//     component.setAllChecked(checked, noPermissions);
+      (
+        permissionsEl.querySelectorAll(
+          'mat-expansion-panel .mat-checkbox-input'
+        )[12] as HTMLElement
+      ).click();
+      fixture.detectChanges();
 
-//     expect(component.rolesBasedPermissions.length).toBe(0);
+      component.permissions$.subscribe((resp) =>
+        expect(resp).toEqual(permissions)
+      );
+      expect(component.permissionsChange.emit).toHaveBeenCalledWith(
+        permissions
+      );
+    });
 
-//     // expect(component.permissionsChange.emit).toHaveBeenCalledWith(
-//     //   component.rolesBasedPermissions
-//     // );
-//   });
+    it('should select `Display Users & Display Roles` when user select `Create Role` permission (module and sub module names are not same)', () => {
+      const permissions: any = cloneDeep(allPermissions);
+      permissions[0].countOfChecked = 11;
+      permissions[1].countOfChecked = 3;
+      permissions[1].permissions[0].checked = true;
+      permissions[1].permissions[4].checked = true;
+      permissions[1].permissions[5].checked = true;
+
+      (
+        permissionsEl.querySelectorAll(
+          'mat-expansion-panel .mat-checkbox-input'
+        )[16] as HTMLElement
+      ).click();
+      fixture.detectChanges();
+
+      component.permissions$.subscribe((resp) =>
+        expect(resp).toEqual(permissions)
+      );
+      expect(component.permissionsChange.emit).toHaveBeenCalledWith(
+        permissions
+      );
+    });
+
+    it('should select `Display Users` when user select `Display Roles` permission (module and sub module names are not same)', () => {
+      const permissions: any = cloneDeep(allPermissions);
+      permissions[0].countOfChecked = 11;
+      permissions[1].countOfChecked = 2;
+      permissions[1].permissions[0].checked = true;
+      permissions[1].permissions[4].checked = true;
+
+      (
+        permissionsEl.querySelectorAll(
+          'mat-expansion-panel .mat-checkbox-input'
+        )[15] as HTMLElement
+      ).click();
+      fixture.detectChanges();
+
+      component.permissions$.subscribe((resp) =>
+        expect(resp).toEqual(permissions)
+      );
+      expect(component.permissionsChange.emit).toHaveBeenCalledWith(
+        permissions
+      );
+    });
+
+    it('should unselect all selected permissions from module when user unselect `Display Dashboard` permission (module and sub module names are same)', () => {
+      const permissions: any = cloneDeep(allPermissions);
+      permissions[0].countOfChecked = 0;
+      permissions[1].countOfChecked = 0;
+      permissions[0].checked = false;
+      permissions[0].permissions.forEach((permission) => {
+        permission.checked = false;
+      });
+
+      (
+        permissionsEl.querySelectorAll(
+          'mat-expansion-panel .mat-checkbox-input'
+        )[0] as HTMLElement
+      ).click();
+      fixture.detectChanges();
+
+      component.permissions$.subscribe((resp) =>
+        expect(resp).toEqual(permissions)
+      );
+      expect(component.permissionsChange.emit).toHaveBeenCalledWith(
+        permissions
+      );
+    });
+
+    it('should unselect all selected permissions from sub module when user unselect `Display Reports` permission (module and sub module names are not same)', () => {
+      const permissions: any = cloneDeep(allPermissions);
+      permissions[0].countOfChecked = 5;
+      permissions[1].countOfChecked = 0;
+      permissions[0].permissions.forEach((permission, index) => {
+        if (index >= 5) {
+          permission.checked = false;
+        }
+      });
+
+      (
+        permissionsEl.querySelectorAll(
+          'mat-expansion-panel .mat-checkbox-input'
+        )[5] as HTMLElement
+      ).click();
+      fixture.detectChanges();
+
+      component.permissions$.subscribe((resp) =>
+        expect(resp).toEqual(permissions)
+      );
+      expect(component.permissionsChange.emit).toHaveBeenCalledWith(
+        permissions
+      );
+    });
+  });
 });
 
+@Component({
+  template: `<app-permissions
+    [allPermissions$]="allPermissions$"
+    [selectedRolePermissions$]="selectedRolePermissions$"
+    [selectedRole]="selectedRole"
+    [roleFormChanged]="roleFormChanged"
+    [isEditable]="isEditable"
+    (permissionsChange)="permissionsChangeHandler($event)"
+    (rolePermissions)="rolePermissionsHandler($event)"
+  ></app-permissions>`
+})
+class TestPermissionsHostComponent {
+  allPermissions$ = allPermissions$;
+  selectedRolePermissions$ = selectedRolePermissions$;
+  selectedRole = allRolesMock[1];
+  roleFormChanged = { isChanged: true };
+  rolesWithPermissionsInUsers = '';
+  isEditable = true;
+  permissionsChangeHandler = () => ({});
+  rolePermissionsHandler = () => ({});
+}
+
+describe('TestPermissionsHostComponent', () => {
+  let component: TestPermissionsHostComponent;
+  let fixture: ComponentFixture<TestPermissionsHostComponent>;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [PermissionsComponent, TestPermissionsHostComponent],
+      imports: [
+        AppMaterialModules,
+        SharedModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        NgxShimmerLoadingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader
+          }
+        })
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestPermissionsHostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
