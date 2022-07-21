569c4ca70eb5cc15052b1453eb7989e15d8d0690
Abhishek Satyanarayanan <E5000664@corpinnovapptive.com>
2022-07-21 17:02:20 +0530

    Fix: Duplicate dashboard names

--- src/app/components/dashboard/dashboard-create-update-dialog/dashboard-create-update-dialog.component.ts
+++ src/app/components/dashboard/dashboard-create-update-dialog/dashboard-create-update-dialog.component.ts
@@ -5,9 +5,12 @@ import {
   OnInit
 } from '@angular/core';
 import {
+  AbstractControl,
+  AsyncValidatorFn,
   FormBuilder,
   FormControl,
   FormGroup,
+  ValidationErrors,
   Validators
 } from '@angular/forms';
 import {
@@ -15,7 +18,10 @@ import {
   MatDialogRef,
   MAT_DIALOG_DATA
 } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, tap, map } from 'rxjs/operators';
 import { Dashboard } from 'src/app/interfaces';
+import { DashboardService } from '../services/dashboard.service';
 
 export interface DashboardCreateUpdateDialogData {
   dialogMode: string;
@@ -31,21 +37,27 @@ export interface DashboardCreateUpdateDialogData {
 export class CreateUpdateDashboardDialogComponent implements OnInit {
   isDefault: boolean | false;
   dashboardForm: FormGroup;
+  existingDashboards: Array<any> = [];
 
   constructor(
     public dialogRef: MatDialogRef<CreateUpdateDashboardDialogComponent>,
     @Inject(MAT_DIALOG_DATA)
     public dialogData: DashboardCreateUpdateDialogData,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private dashboardService: DashboardService
   ) {}
 
   ngOnInit(): void {
     this.dashboardForm = this.fb.group({
-      dashboardName: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(20)
-      ])
+      dashboardName: new FormControl(
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20)
+        ],
+        this.checkIfDashboardNameExists()
+      )
     });
     if (this.dialogData.dialogMode === 'EDIT') {
       this.f.dashboardName.setValue(this.dialogData.data.name);
@@ -55,6 +67,23 @@ export class CreateUpdateDashboardDialogComponent implements OnInit {
 
   get f() {
     return this.dashboardForm.controls;
+  }
+
+  checkIfDashboardNameExists(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> =>
+      this.dashboardService.getDashboards$().pipe(
+        debounceTime(1000),
+        distinctUntilChanged(),
+        map((dashboards) => {
+          const forbidden =
+            dashboards.findIndex(
+              (db: any) => db.name === control.value.trim()
+            ) >= 0;
+          return forbidden
+            ? { exists: true, existingName: control.value.trim() }
+            : null;
+        })
+      );
   }
 
   onNoClick(): void {
