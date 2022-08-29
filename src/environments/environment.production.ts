3b547f1154cd079082e0e7f1f7b92f3cf52d9e04
Kiran Kumar Palani <kiran.palani@innovapptive.com>
2022-08-29 17:48:37 +0530

    updated prod domain names

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
-  dashboardApiUrl: 'https://beta.cbo.innovapptive.com/dashboardapi/',
-  mccAbapApiUrl: 'https://beta.cbo.innovapptive.com/mccspccabapapi/',
-  spccAbapApiUrl: 'https://beta.cbo.innovapptive.com/mccspccabapapi/',
-  wiApiUrl: 'https://beta.cbo.innovapptive.com/wiapi/',
-  wiAbapApiUrl: 'https://beta.cbo.innovapptive.com/wiabapapi/',
+  dashboardApiUrl: 'https://cbo.innovapptive.com/dashboardapi/',
+  mccAbapApiUrl: 'https://cbo.innovapptive.com/mccspccabapapi/',
+  spccAbapApiUrl: 'https://cbo.innovapptive.com/mccspccabapapi/',
+  wiApiUrl: 'https://cbo.innovapptive.com/wiapi/',
+  wiAbapApiUrl: 'https://cbo.innovapptive.com/wiabapapi/',
   userRoleManagementApiUrl:
-    'https://beta.cbo.innovapptive.com/userrolemanagementapi/',
-  slackAPIUrl: 'https://beta.cbo.innovapptive.com/userrolemanagementapi/slack/',
+    'https://cbo.innovapptive.com/userrolemanagementapi/',
+  slackAPIUrl: 'https://cbo.innovapptive.com/userrolemanagementapi/slack/',
   s3BaseUrl: 'https://innovwi.s3.ap-south-1.amazonaws.com/',
   requestTimeout: 60000 * 3,
   undoRedoOffset: 5
