54a991ac7c3f8fff4ce31f0c9cc205aff6896f0f
Abhishek Satyanarayanan <abhishek.satyanarayanan@innovapptive.com>
2022-09-06 19:02:12 +0530

    Fixed toastr placement

--- src/app/shared/toast/toast-ref.ts
+++ src/app/shared/toast/toast-ref.ts
@@ -3,15 +3,18 @@ import { OverlayRef } from '@angular/cdk/overlay';
 
 // @ts-ignore
 export class ToastRef {
-  constructor(private readonly overlay: OverlayRef) {
-  }
+  constructor(private readonly overlay: OverlayRef) {}
 
   close() {
     this.overlay.dispose();
   }
 
   isVisible() {
-    return this.overlay && this.overlay.overlayElement;
+    return (
+      this.overlay &&
+      this.overlay.overlayElement &&
+      window.getComputedStyle(this.overlay.overlayElement).display === 'none'
+    );
   }
 
   getPosition() {
