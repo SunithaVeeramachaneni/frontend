5406f4aada554312afa879b38939d59555c155eb
Shiva Kanneboina <E5000557@corpinnovapptive.com>
2022-09-15 11:50:28 +0530

    sonar cube issue resolved

--- src/app/shared/utils/waitFor.ts
+++ src/app/shared/utils/waitFor.ts
@@ -9,17 +9,25 @@ async function waitFor(promise: Promise<any> | Observable<any>): Promise<any> {
   if (isObservable(promise)) {
     promise = promise.pipe(take(1)).toPromise();
   }
-  const macroTask = Zone.current.scheduleMacroTask(
-    `WAITFOR-${Math.random()}`,
-    () => {},
-    {},
-    () => {}
-  );
 
-  return promise.then((p) => {
-    macroTask.invoke();
-    return p;
-  });
+  if (window.crypto && window.crypto.getRandomValues) {
+    const random =
+      window.crypto.getRandomValues(new Uint32Array(1))[0] / 2 ** 32;
+    const macroTask = Zone.current.scheduleMacroTask(
+      `WAITFOR-${random}`,
+      () => {},
+      {},
+      () => {}
+    );
+
+    return promise.then((p) => {
+      macroTask.invoke();
+      return p;
+    });
+  } else
+    throw new Error(
+      'Your browser can not generate a secure random number, please change it to one that can (Google Chrome, IE, Firefox, Safari, Opera) or get the newest update.'
+    );
 }
 
 export default waitFor;
