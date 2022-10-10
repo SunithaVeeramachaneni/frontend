9c1513a9006891f24ac7352a1c7b2fcb703fa067
Kiran Kumar Palani <kiran.palani@innovapptive.com>
2022-10-10 19:38:10 +0530

    enabling add question & section based on active section

--- src/app/components/race-dynamic-forms/create-form/create-form.component.ts
+++ src/app/components/race-dynamic-forms/create-form/create-form.component.ts
@@ -76,10 +76,10 @@ export class CreateFormComponent implements OnInit, AfterViewInit {
     max: 100,
     increment: 1
   };
-  isPopoverOpen = [false];
   popOverOpenState = {};
   fieldContentOpenState = {};
   richTextEditorToolbarState = {};
+  sectionActiveState = {};
   isLLFFieldChanged = false;
   sections: any;
 
@@ -241,6 +241,7 @@ export class CreateFormComponent implements OnInit, AfterViewInit {
           const { uid, name, position, questions } = section;
           const sc = index + 1;
           if (!this.isOpenState[sc]) this.isOpenState[sc] = true;
+          if (!this.sectionActiveState[sc]) this.sectionActiveState[sc] = false;
           if (!this.fieldContentOpenState[sc])
             this.fieldContentOpenState[sc] = {};
           if (!this.popOverOpenState[sc]) this.popOverOpenState[sc] = {};
@@ -297,6 +298,7 @@ export class CreateFormComponent implements OnInit, AfterViewInit {
         this.disableFormFields = false;
       }
     });
+    this.sectionActiveState[1] = true;
   }
 
   setHeaderTitle(title) {
@@ -505,6 +507,7 @@ export class CreateFormComponent implements OnInit, AfterViewInit {
 
   initSection = (sc: number, qc: number, uqc: number) => {
     if (!this.isOpenState[sc]) this.isOpenState[sc] = true;
+    if (!this.sectionActiveState[sc]) this.sectionActiveState[sc] = false;
     if (!this.fieldContentOpenState[sc]) this.fieldContentOpenState[sc] = {};
     if (!this.popOverOpenState[sc]) this.popOverOpenState[sc] = {};
     if (!this.richTextEditorToolbarState[sc])
@@ -707,4 +710,11 @@ export class CreateFormComponent implements OnInit, AfterViewInit {
   }
 
   getLogicsFormBuilderArray(logics) {}
+
+  activeSection(sectionIndex: number) {
+    Object.keys(this.sectionActiveState).forEach((key) => {
+      this.sectionActiveState[key] = false;
+    });
+    this.sectionActiveState[sectionIndex + 1] = true;
+  }
 }
