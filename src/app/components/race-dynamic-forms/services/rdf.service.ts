ff31ef89127d95b7d22187db0b65439778d674c9
Shiva Kanneboina <E5000557@corpinnovapptive.com>
2022-10-12 12:54:40 +0530

    expression build logic order changed

--- src/app/components/race-dynamic-forms/services/rdf.service.ts
+++ src/app/components/race-dynamic-forms/services/rdf.service.ts
@@ -311,11 +311,11 @@ export class RdfService {
         if (isEmpty) {
           expression = `${expression}${
             index + 1
-          }:(E) ${questionId} EQ MANDIT IF ${mq} ${logic.operator} EMPTY`;
+          }:(E) ${mq} EQ MANDIT IF ${questionId} ${logic.operator} EMPTY`;
         } else {
           expression = `${expression}${
             index + 1
-          }:(E) ${questionId} EQ MANDIT IF ${mq} ${logic.operator} (V)${
+          }:(E) ${mq} EQ MANDIT IF ${questionId} ${logic.operator} (V)${
             logic.operand2
           }`;
         }
@@ -332,11 +332,11 @@ export class RdfService {
           expression = `${expression};`;
         }
         if (isEmpty) {
-          expression = `${expression}${index + 1}:(HI) ${questionId} IF ${hq} ${
+          expression = `${expression}${index + 1}:(HI) ${hq} IF ${questionId} ${
             logic.operator
           } EMPTY`;
         } else {
-          expression = `${expression}${index + 1}:(HI) ${questionId} IF ${hq} ${
+          expression = `${expression}${index + 1}:(HI) ${hq} IF ${questionId} ${
             logic.operator
           } (V)${logic.operand2}`;
         }
