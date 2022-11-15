f640ae1c5f639abba8c03acc5ac246d4ad9749cf
Kiran Kumar Palani <kiran.palani@innovapptive.com>
2022-11-15 14:51:19 +0530

    fixed failing unit test case

--- src/app/components/work-instructions/player/player.component.spec.ts
+++ src/app/components/work-instructions/player/player.component.spec.ts
@@ -1,21 +1,29 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { PlyrModule } from 'ngx-plyr';
 import { AppMaterialModules } from '../../../material.module';
+import { TenantService } from '../../tenant-management/services/tenant.service';
 
 import { PlayerComponent } from './player.component';
 
 describe('PlayerComponent', () => {
   let component: PlayerComponent;
   let fixture: ComponentFixture<PlayerComponent>;
+  let tenantServiceSpy: TenantService;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [PlayerComponent],
-        imports: [AppMaterialModules, PlyrModule]
-      }).compileComponents();
-    })
-  );
+  beforeEach(waitForAsync(() => {
+    tenantServiceSpy = jasmine.createSpyObj('TenantService', ['getTenantInfo']);
+
+    TestBed.configureTestingModule({
+      declarations: [PlayerComponent],
+      imports: [AppMaterialModules, PlyrModule],
+      providers: [
+        {
+          provide: TenantService,
+          useValue: tenantServiceSpy
+        }
+      ]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PlayerComponent);
