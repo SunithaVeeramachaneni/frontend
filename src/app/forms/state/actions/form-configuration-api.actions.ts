4c6cf030eed9474a5c384b79f7f7de2c5f2f4031
Kiran Kumar Palani <kiran.palani@innovapptive.com>
2023-01-08 10:36:18 +0530

    Addressed review comments & linked edit template

--- src/app/forms/state/actions/form-configuration-api.actions.ts
+++ src/app/forms/state/actions/form-configuration-api.actions.ts
@@ -1,5 +1,11 @@
 import { createAction, props } from '@ngrx/store';
-import { FormMetadata, Page } from 'src/app/interfaces';
+import {
+  CreateAuthoredFormDetailMutation,
+  CreateFormDetailMutation,
+  UpdateAuthoredFormDetailMutation,
+  UpdateFormDetailMutation
+} from 'src/app/API.service';
+import { FormMetadata } from 'src/app/interfaces';
 
 export const createFormSuccess = createAction(
   '[Form Configuration Service] createFormSuccess',
@@ -13,7 +19,7 @@ export const createFormFailure = createAction(
 
 export const updateFormSuccess = createAction(
   '[Form Configuration Service] updateFormSuccess',
-  props<{ formMetadata: any; formSaveStatus: string }>()
+  props<{ formMetadata: FormMetadata; formSaveStatus: string }>()
 );
 
 export const updateFormFailure = createAction(
@@ -24,7 +30,7 @@ export const updateFormFailure = createAction(
 export const createFormDetailSuccess = createAction(
   '[Form Configuration Component] createFormDetailSuccess',
   props<{
-    formDetail: any;
+    formDetail: CreateFormDetailMutation;
     formPublishStatus: string;
   }>()
 );
@@ -39,7 +45,7 @@ export const createFormDetailFailure = createAction(
 export const updateFormDetailSuccess = createAction(
   '[Form Configuration Component] updateFormDetailSuccess',
   props<{
-    formDetail: any;
+    formDetail: UpdateFormDetailMutation;
     formPublishStatus: string;
   }>()
 );
@@ -53,7 +59,10 @@ export const updateFormDetailFailure = createAction(
 
 export const createAuthoredFromDetailSuccess = createAction(
   '[Form Configuration Service] createAuthoredFromDetailSuccess',
-  props<{ authoredFormDetail: any; formSaveStatus: string }>()
+  props<{
+    authoredFormDetail: CreateAuthoredFormDetailMutation;
+    formSaveStatus: string;
+  }>()
 );
 
 export const createAuthoredFromDetailFailure = createAction(
@@ -63,7 +72,10 @@ export const createAuthoredFromDetailFailure = createAction(
 
 export const updateAuthoredFromDetailSuccess = createAction(
   '[Form Configuration Service] updateAuthoredFromDetailSuccess',
-  props<{ authoredFormDetail: any; formSaveStatus: string }>()
+  props<{
+    authoredFormDetail: UpdateAuthoredFormDetailMutation;
+    formSaveStatus: string;
+  }>()
 );
 
 export const updateAuthoredFromDetailFailure = createAction(
