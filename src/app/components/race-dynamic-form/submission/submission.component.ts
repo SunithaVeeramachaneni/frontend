8627c08342f331e4b804df9c11df29e033ae12bd
Amit Malviya <amit.malviya@innovapptive.com>
2023-01-10 16:54:15 +0530

    Style: UI fixes

--- src/app/components/race-dynamic-form/submission/submission.component.ts
+++ src/app/components/race-dynamic-form/submission/submission.component.ts
@@ -52,7 +52,16 @@ export class SubmissionComponent implements OnInit, OnDestroy {
         color: 'darkgray'
       },
       hasPreTextImage: true,
-      hasPostTextImage: false
+      preTextImageConfig: {
+        logoAvialable: false,
+        style: {
+          width: '40px',
+          height: '40px',
+          marginRight: '10px'
+        }
+      },
+      hasPostTextImage: false,
+      postTextImageConfig: {}
     },
     {
       id: 'status',
@@ -71,24 +80,19 @@ export class SubmissionComponent implements OnInit, OnDestroy {
       sticky: false,
       groupable: true,
       titleStyle: {
-        fontFamily: 'Inter',
-        fontStyle: 'normal',
-        fontSize: '14px',
         textTransform: 'capitalize',
         fontWeight: 500,
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
-        padding: '0 12px',
-        marginTop: '8px',
+        position: 'relative',
+        top: '10px',
         width: '90px',
-        height: '24px',
-        background: '#D1FAE5', // #FEF3C7
-        borderRadius: '12px',
-        flex: 'none',
-        order: 0,
-        flexGrow: 0
+        height: '25px',
+        background: '#FEF3C7',
+        color: '#92400E',
+        borderRadius: '12px'
       },
       subtitleStyle: {},
       hasPreTextImage: false,
@@ -183,7 +187,7 @@ export class SubmissionComponent implements OnInit, OnDestroy {
   configOptions: ConfigOptions = {
     tableID: 'formsTable',
     rowsExpandable: false,
-    enableRowsSelection: true,
+    enableRowsSelection: false,
     enablePagination: false,
     displayFilterPanel: false,
     displayActionsColumn: false,
