2f1a4ddff3e617cea53d8003b5674f6726255100
Kaushik Parmar <kaushik.parmar.sa@gmail.com>
2023-01-18 17:24:01 +0530

    [fix] comment

--- src/app/forms/components/question/question.component.ts
+++ src/app/forms/components/question/question.component.ts
@@ -194,6 +194,11 @@ export class QuestionComponent implements OnInit {
       .pipe(
         tap((question) => {
           if (question) {
+            //  if (question.isOpen) {
+            //    timer(0).subscribe(() => this.name.nativeElement.focus());
+            //  } else {
+            //    timer(0).subscribe(() => this.name.nativeElement.blur());
+            //  }
             this.question = question;
             this.questionForm.patchValue(question, {
               emitEvent: false
