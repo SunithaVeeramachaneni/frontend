999a403ec6d06b456334e176da0fff2e0b5c3827
Amit M <amit.malviya@innovapptive.com>
2023-02-09 15:26:32 +0530

    Add: add uom form

--- src/app/components/master-configurations/unit-measurement/add-edit-uom/add-edit-uom.component.ts
+++ src/app/components/master-configurations/unit-measurement/add-edit-uom/add-edit-uom.component.ts
@@ -7,12 +7,14 @@ import {
   ChangeDetectionStrategy
 } from '@angular/core';
 import {
+  FormArray,
   FormBuilder,
   FormControl,
   FormGroup,
   Validators
 } from '@angular/forms';
 import { ValidationError } from 'src/app/interfaces';
+import { WhiteSpaceValidator } from 'src/app/shared/validators/white-space-validator';
 
 @Component({
   selector: 'app-add-edit-uom',
@@ -20,7 +22,7 @@ import { ValidationError } from 'src/app/interfaces';
   styleUrls: ['./add-edit-uom.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AddEditUnitOfMeasurementComponent {
+export class AddEditUnitOfMeasurementComponent implements OnInit {
   @Output() slideInOut: EventEmitter<any> = new EventEmitter();
   @Output() createUnitData: EventEmitter<any> = new EventEmitter();
   @Input() set unitEditData(data) {
@@ -28,8 +30,70 @@ export class AddEditUnitOfMeasurementComponent {
     this._unitEditData = data;
   }
   _unitEditData: any;
+  public unitOfMeasurement: string;
+  public measurementList: any[];
+  errors: ValidationError = {};
+  public unitMeasurment: FormGroup;
+  public units: FormArray;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.measurementList = ["Length","Area","Volume","Temperature"];
+    this.unitMeasurment = new FormGroup({
+      units: new FormArray([])
+    });
+  }
 
   cancel() {
     this.slideInOut.emit('out');
+  }
+
+  onSave() {
+    this.unitMeasurment.get('units').value;
+  }
+
+  onMeasurementChange = ($event) => {
+    console.log($event);
+    this.addNewUmo();
+  }
+
+  createUnit(): FormGroup {
+    return this.formBuilder.group({
+      description:  [ '',[
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(48),
+        WhiteSpaceValidator.whiteSpace,
+        WhiteSpaceValidator.trimWhiteSpace
+      ]],
+      symbol:  [ '',[
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(48),
+        WhiteSpaceValidator.whiteSpace,
+        WhiteSpaceValidator.trimWhiteSpace
+      ]],
+    });
+  }
+  
+  addNewUmo(): void {
+    this.units = this.unitMeasurment.get('units') as FormArray;
+    this.units.push(this.createUnit());
+  }
+
+  processValidationErrors(index:any, controlName: string): boolean {
+    const touched = (<FormArray> this.unitMeasurment.get('units')).at(index).get(controlName).touched;
+    const errors = (<FormArray> this.unitMeasurment.get('units')).at(index).get(controlName).errors;
+    this.errors[controlName] = null;
+    if (touched && errors) {
+      Object.keys(errors).forEach((messageKey) => {
+        this.errors[controlName] = {
+          name: messageKey,
+          length: errors[messageKey]?.requiredLength
+        };
+      });
+    }
+    return !touched || this.errors[controlName] === null ? false : true;
   }
 }
