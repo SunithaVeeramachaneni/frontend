88db9353cf45663428a6b2a78a98e5e71bf402a3
Abhishek Satyanarayanan <abhishek.satyanarayanan@innovapptive.com>
2023-02-17 17:07:12 +0530

    Show hierarchy pop over component

---
+++ src/app/forms/components/show-hierarchy-popup/show-hierarchy-popup.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { AssetHierarchyUtil } from 'src/app/shared/utils/assetHierarchyUtil';
+
+import { hierarchyMock } from 'src/app/forms/components/utils/utils';
+import { HierarchyEntity } from 'src/app/interfaces';
+
+@Component({
+  selector: 'app-show-hierarchy-popup',
+  templateUrl: './show-hierarchy-popup.component.html',
+  styleUrls: ['./show-hierarchy-popup.component.scss']
+})
+export class ShowHierarchyPopupComponent implements OnInit {
+  public hierarchyList: any[];
+  public hierarchyToBeDisplayed = {} as HierarchyEntity;
+
+  constructor(private assetHierarchyUtil: AssetHierarchyUtil) {}
+
+  ngOnInit(): void {
+    this.hierarchyList =
+      this.assetHierarchyUtil.prepareHierarchyList(hierarchyMock);
+    console.log(this.hierarchyList);
+
+    this.hierarchyToBeDisplayed = this.assetHierarchyUtil.getHierarchyByNodeId(
+      this.hierarchyList,
+      'Subchild Node 1'
+    );
+    console.log(this.hierarchyToBeDisplayed);
+  }
+}
