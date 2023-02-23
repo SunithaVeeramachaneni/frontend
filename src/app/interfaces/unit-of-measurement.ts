f4cfcd8d80b6c4c83e6f0abef87802d7e0cb7492
Amit Malviya <amit.malviya@innovapptive.com>
2023-02-23 22:26:36 +0530

    Fix: Update interface

--- src/app/interfaces/unit-of-measurement.ts
+++ src/app/interfaces/unit-of-measurement.ts
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 export type ListUnitMeasumentsQuery = {
-  __typename: 'ModelUnitMeasumentConnection';
   items: Array<{
-    __typename: 'UnitMeasument';
     id: string;
     description?: string | null;
     symbol?: string | null;
@@ -14,15 +12,12 @@ export type ListUnitMeasumentsQuery = {
     createdAt: string;
     updatedAt: string;
     _version: number;
-    _deleted?: boolean | null;
-    _lastChangedAt: number;
   } | null>;
   nextToken?: string | null;
   startedAt?: number | null;
 };
 
 export type GetUnitMeasumentQuery = {
-  __typename: 'UnitMeasument';
   id: string;
   description?: string | null;
   symbol?: string | null;
@@ -31,20 +26,15 @@ export type GetUnitMeasumentQuery = {
   unitlistID: string;
   searchTerm?: string | null;
   unitList?: {
-    __typename: 'UnitList';
     id: string;
     name?: string | null;
     isDeleted?: boolean | null;
     createdAt: string;
     updatedAt: string;
     _version: number;
-    _deleted?: boolean | null;
-    _lastChangedAt: number;
   } | null;
   isActive?: boolean | null;
   createdAt: string;
   updatedAt: string;
   _version: number;
-  _deleted?: boolean | null;
-  _lastChangedAt: number;
 };
