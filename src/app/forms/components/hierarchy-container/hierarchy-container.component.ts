b3b7ba003ee65dd0c353203f920857debce70eed
Shiva Kanneboina <E5000557@corpinnovapptive.com>
2023-02-23 18:05:39 +0530

    node image logo

--- src/app/forms/components/hierarchy-container/hierarchy-container.component.ts
+++ src/app/forms/components/hierarchy-container/hierarchy-container.component.ts
@@ -61,6 +61,10 @@ export class HierarchyContainerComponent implements OnInit {
           this.totalAssetsCount =
             assetHierarchyUtil.getTotalAssetCount(selectedHierarchy);
           this.hierarchy = JSON.parse(JSON.stringify(selectedHierarchy));
+
+          const stitchedHierarchy =
+            assetHierarchyUtil.prepareAssetHierarchy(selectedHierarchy);
+
           this.filteredHierarchyList = JSON.parse(
             JSON.stringify(this.hierarchy)
           );
