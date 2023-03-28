f877b528aa0e4cbd1f1ddafcbb5d010d7f5ae0d0
Amit Malviya <amit.malviya@innovapptive.com>
2023-03-28 10:20:50 +0530

    Fix: Console error

--- src/app/interfaces/operator-rounds.ts
+++ src/app/interfaces/operator-rounds.ts
@@ -18,6 +18,7 @@ export interface RoundPlanScheduleConfiguration {
   assignmentDetails: AssignmentDetail;
   advanceRoundsCount: number;
   startDatePicker?: Date;
+  endDatePicker?: Date;
   scheduleEndOnPicker?: Date;
 }
 
