9fe82af08116ce49e3eaabd25f29583a5907b377
Abhishek Satyanarayanan <abhishek.satyanarayanan@innovapptive.com>
2023-04-03 13:00:11 +0530

    Fixed current date and time

--- src/app/components/race-dynamic-forms/services/rdf.service.ts
+++ src/app/components/race-dynamic-forms/services/rdf.service.ts
@@ -312,19 +312,19 @@ export class RdfService {
 
   getProperties(question, formId = null) {
     let properties = {};
-    const { fieldType, readOnly } = question;
+    const { fieldType, readOnly, value } = question;
     switch (fieldType) {
       case 'DF': {
         properties = {
           ...properties,
-          DEFAULTVALUE: 'CD'
+          DEFAULTVALUE: value ? 'CD' : '' // Confirm from mobile
         };
         break;
       }
       case 'TIF': {
         properties = {
           ...properties,
-          DEFAULTVALUE: 'CT'
+          DEFAULTVALUE: value ? 'CT' : '' // Confirm from mobile
         };
         break;
       }
