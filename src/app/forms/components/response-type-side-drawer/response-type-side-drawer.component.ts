114f284a6cb0bd801a1e4308c74bfccd2ea20fc2
Priyanka Singh <priyanka.singh@innovapptive.com>
2023-04-04 18:00:54 +0530

    response pop up UI fixed

--- src/app/forms/components/response-type-side-drawer/response-type-side-drawer.component.ts
+++ src/app/forms/components/response-type-side-drawer/response-type-side-drawer.component.ts
@@ -199,7 +199,7 @@ export class ResponseTypeSideDrawerComponent implements OnInit {
     this.formService.setMultiChoiceOpenState({ isOpen: false, response: {} });
   };
 
-  printColor(ev, response) {
+  applyBGColor(ev, response) {
     const color = ev.target.value;
     const r = parseInt(color.substr(1, 2), 16);
     const g = parseInt(color.substr(3, 2), 16);
