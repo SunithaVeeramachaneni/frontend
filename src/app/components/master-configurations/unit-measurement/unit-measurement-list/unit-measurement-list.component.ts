45be78bc65c6687872718554237f89503543ef5c
Priyanka Singh <priyanka.singh@innovapptive.com>
2023-04-15 21:57:14 +0530

    icon changes fixed

--- src/app/components/master-configurations/unit-measurement/unit-measurement-list/unit-measurement-list.component.ts
+++ src/app/components/master-configurations/unit-measurement/unit-measurement-list/unit-measurement-list.component.ts
@@ -218,7 +218,6 @@ export class UnitMeasurementListComponent implements OnInit {
   skip = 0;
   limit = defaultLimit;
   searchUom: FormControl;
-  closeIcon = 'assets/img/svg/cancel-icon.svg';
   ghostLoading = new Array(12).fill(0).map((v, i) => i);
   nextToken = '';
   fetchType = 'load';
