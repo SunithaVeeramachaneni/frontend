24deabda2314a86ca11ee41c8553a4b6d86f8f71
Amit Malviya <amit.malviya@innovapptive.com>
2023-04-17 23:41:44 +0530

    Fix: Publish round param changes

--- src/app/forms/state/round-plan-configuration.effects.ts
+++ src/app/forms/state/round-plan-configuration.effects.ts
@@ -95,7 +95,8 @@ export class RoundPlanConfigurationEffects {
                   pages: JSON.stringify(authoredFormDetail.pages),
                   subForms, // Handle subforms form round-plan config,
                   hierarchy
-                }
+                },
+                isEdit: this.operatorRoundsService.isEdit
               })
               .pipe(
                 map((createAuthoredFormDetail) =>
