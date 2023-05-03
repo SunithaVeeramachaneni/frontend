8c1a2bb393371826ffb945d2f3809fe9ddc29b73
Kiran Kumar Palani <kiran.palani@innovapptive.com>
2023-05-03 20:58:00 +0530

    s3 image url update for slide show

--- src/app/components/operator-rounds/issues-actions-detail-view/issues-actions-detail-view.component.ts
+++ src/app/components/operator-rounds/issues-actions-detail-view/issues-actions-detail-view.component.ts
@@ -365,7 +365,7 @@ export class IssuesActionsDetailViewComponent
   openPreviewDialog() {
     const slideshowImages = [];
     this.filteredMediaType.forEach((media) => {
-      slideshowImages.push(this.s3BaseUrl + media.message);
+      slideshowImages.push(`${this.s3BaseUrl}public/${media.message}`);
     });
     if (slideshowImages) {
       this.dialog.open(SlideshowComponent, {
