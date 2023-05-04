f86b4b63c96c15012c39b0846850ab7c365837b6
Shaik Mohammed Shaquib <shaquib.shaikh@innovapptive.com>
2023-05-04 11:23:31 +0530

    Created issuelist component in form shared module

---
+++ src/app/forms/components/issues-list/issues-list.component.ts
@@ -0,0 +1,546 @@
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnInit
+} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import {
+  Column,
+  ConfigOptions
+} from '@innovapptive.com/dynamictable/lib/interfaces';
+import { format } from 'date-fns';
+import {
+  BehaviorSubject,
+  combineLatest,
+  Observable,
+  of,
+  ReplaySubject
+} from 'rxjs';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  mergeMap,
+  switchMap,
+  tap
+} from 'rxjs/operators';
+import { slideInOut } from 'src/app/animations';
+
+import { defaultLimit, permissions as perms } from 'src/app/app.constants';
+import {
+  AssigneeDetails,
+  CellClickActionEvent,
+  LoadEvent,
+  Permission,
+  RowLevelActionEvent,
+  SearchEvent,
+  TableEvent,
+  UserDetails,
+  UserInfo
+} from 'src/app/interfaces';
+import { GetFormList } from 'src/app/interfaces/master-data-management/forms';
+import { LoginService } from 'src/app/components/login/services/login.service';
+import { IssuesActionsViewComponent } from '../issues-actions-view/issues-actions-view.component';
+import { RoundPlanObservationsService } from 'src/app/components/operator-rounds/services/round-plan-observation.service';
+import { UsersService } from 'src/app/components/user-management/services/users.service';
+
+@Component({
+  selector: 'app-issues-list',
+  templateUrl: './issues-list.component.html',
+  styleUrls: ['./issues-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [slideInOut]
+})
+export class IssuesListComponent implements OnInit {
+  @Input() set users$(users$: Observable<UserDetails[]>) {
+    this._users$ = users$.pipe(
+      tap((users) => (this.assigneeDetails = { users }))
+    );
+  }
+  get users$(): Observable<UserDetails[]> {
+    return this._users$;
+  }
+  assigneeDetails: AssigneeDetails;
+  columns: Column[] = [
+    {
+      id: 'title',
+      displayName: 'Title',
+      type: 'string',
+      controlType: 'string',
+      order: 1,
+      searchable: false,
+      sortable: false,
+      hideable: false,
+      visible: true,
+      movable: false,
+      stickable: false,
+      sticky: false,
+      groupable: false,
+      titleStyle: {
+        'font-weight': '500',
+        'font-size': '100%',
+        color: '#000000',
+        'overflow-wrap': 'anywhere'
+      },
+      hasSubtitle: true,
+      showMenuOptions: false,
+      subtitleColumn: 'description',
+      subtitleStyle: {
+        'font-size': '80%',
+        color: 'darkgray',
+        'overflow-wrap': 'anywhere'
+      },
+      hasPreTextImage: true,
+      hasPostTextImage: false
+    },
+    {
+      id: 'locationAsset',
+      displayName: 'Location/Asset',
+      type: 'string',
+      controlType: 'string',
+      order: 2,
+      searchable: false,
+      sortable: true,
+      hideable: false,
+      visible: true,
+      movable: false,
+      stickable: false,
+      sticky: false,
+      groupable: false,
+      titleStyle: {
+        'font-size': '100%'
+      },
+      hasSubtitle: true,
+      showMenuOptions: false,
+      subtitleColumn: 'locationAssetDescription',
+      subtitleStyle: {
+        'font-size': '80%',
+        color: 'darkgray'
+      },
+      hasPreTextImage: false,
+      hasPostTextImage: false
+    },
+    {
+      id: 'plant',
+      displayName: 'Plant',
+      type: 'string',
+      controlType: 'string',
+      order: 3,
+      hasSubtitle: false,
+      showMenuOptions: false,
+      subtitleColumn: '',
+      searchable: false,
+      sortable: true,
+      hideable: false,
+      visible: true,
+      movable: false,
+      stickable: false,
+      sticky: false,
+      groupable: true,
+      titleStyle: {},
+      subtitleStyle: {},
+      hasPreTextImage: false,
+      hasPostTextImage: false
+    },
+    {
+      id: 'priority',
+      displayName: 'Priority',
+      type: 'number',
+      controlType: 'string',
+      order: 4,
+      hasSubtitle: false,
+      showMenuOptions: false,
+      subtitleColumn: '',
+      searchable: false,
+      sortable: true,
+      hideable: false,
+      visible: true,
+      movable: false,
+      stickable: false,
+      sticky: false,
+      groupable: true,
+      titleStyle: {
+        textTransform: 'capitalize',
+        fontWeight: 500,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+        top: '10px',
+        height: '24px',
+        color: '#ff4033',
+        borderRadius: '12px'
+      },
+      subtitleStyle: {},
+      hasPreTextImage: false,
+      hasPostTextImage: false,
+      hasConditionalStyles: true
+    },
+    {
+      id: 'status',
+      displayName: 'Status',
+      type: 'string',
+      controlType: 'string',
+      order: 5,
+      hasSubtitle: false,
+      showMenuOptions: false,
+      subtitleColumn: '',
+      searchable: false,
+      sortable: true,
+      hideable: false,
+      visible: true,
+      movable: false,
+      stickable: false,
+      sticky: false,
+      groupable: true,
+      titleStyle: {
+        textTransform: 'capitalize',
+        fontWeight: 500,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+        top: '10px',
+        width: '90px',
+        right: '15px',
+        height: '24px',
+        background: '#FEF3C7',
+        color: '#92400E',
+        borderRadius: '12px'
+      },
+      subtitleStyle: {},
+      hasPreTextImage: false,
+      hasPostTextImage: false,
+      hasConditionalStyles: true
+    },
+    {
+      id: 'dueDate',
+      displayName: 'Due Date',
+      type: 'string',
+      controlType: 'string',
+      order: 6,
+      hasSubtitle: false,
+      showMenuOptions: false,
+      subtitleColumn: '',
+      searchable: false,
+      sortable: true,
+      hideable: false,
+      visible: true,
+      movable: false,
+      stickable: false,
+      sticky: false,
+      groupable: true,
+      titleStyle: {},
+      subtitleStyle: {},
+      hasPreTextImage: false,
+      hasPostTextImage: false
+    },
+    {
+      id: 'notificationNumber',
+      displayName: 'Notification No.',
+      type: 'string',
+      controlType: 'string',
+      order: 7,
+      hasSubtitle: false,
+      showMenuOptions: false,
+      subtitleColumn: '',
+      searchable: false,
+      sortable: true,
+      hideable: false,
+      visible: true,
+      movable: false,
+      stickable: false,
+      sticky: false,
+      groupable: true,
+      titleStyle: {},
+      subtitleStyle: {},
+      hasPreTextImage: false,
+      hasPostTextImage: false
+    },
+    {
+      id: 'assignedTo',
+      displayName: 'Assigned To',
+      type: 'string',
+      controlType: 'string',
+      order: 8,
+      hasSubtitle: false,
+      showMenuOptions: false,
+      subtitleColumn: '',
+      searchable: false,
+      sortable: true,
+      hideable: false,
+      visible: true,
+      movable: false,
+      stickable: false,
+      sticky: false,
+      groupable: true,
+      titleStyle: {
+        'overflow-wrap': 'anywhere'
+      },
+      subtitleStyle: {},
+      hasPreTextImage: false,
+      hasPostTextImage: false
+    }
+  ];
+  configOptions: ConfigOptions = {
+    tableID: 'issuesTable',
+    rowsExpandable: false,
+    enableRowsSelection: false,
+    enablePagination: false,
+    displayFilterPanel: false,
+    displayActionsColumn: false,
+    rowLevelActions: {
+      menuActions: []
+    },
+    groupByColumns: [],
+    pageSizeOptions: [10, 25, 50, 75, 100],
+    allColumns: [],
+    tableHeight: 'calc(100vh - 390px)',
+    groupLevelColors: ['#e7ece8', '#c9e3e8', '#e8c9c957'],
+    conditionalStyles: {
+      high: {
+        color: '#FF3B30'
+      },
+      medium: {
+        color: '#FF9500'
+      },
+      low: {
+        color: '#8A8A8C'
+      },
+      open: {
+        'background-color': '#F56565',
+        color: '#ffffff'
+      },
+      'in-progress': {
+        'background-color': '#FFCC00',
+        color: '#000000'
+      },
+      resolved: {
+        'background-color': '#2C9E53',
+        color: '#FFFFFF'
+      }
+    }
+  };
+
+  dataSource: MatTableDataSource<any>;
+  issues$: Observable<{
+    columns: Column[];
+    data: any[];
+  }>;
+  fetchIssues$: ReplaySubject<TableEvent | LoadEvent | SearchEvent> =
+    new ReplaySubject<TableEvent | LoadEvent | SearchEvent>(2);
+  skip = 0;
+  limit = defaultLimit;
+  searchIssue: FormControl;
+  nextToken = '';
+  menuState = 'out';
+  ghostLoading = new Array(12).fill(0).map((v, i) => i);
+  fetchType = 'load';
+  isLoading$: BehaviorSubject<boolean> = new BehaviorSubject(true);
+  userInfo$: Observable<UserInfo>;
+  issuesCount$: Observable<number>;
+  initial: any;
+  isModalOpened = false;
+  readonly perms = perms;
+  private _users$: Observable<UserDetails[]>;
+
+  constructor(
+    private readonly roundPlanObservationsService: RoundPlanObservationsService,
+    private readonly loginService: LoginService,
+    private readonly userService: UsersService,
+    private dialog: MatDialog,
+    private cdrf: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.fetchIssues$.next({ data: 'load' });
+    this.fetchIssues$.next({} as TableEvent);
+    this.searchIssue = new FormControl('');
+    this.searchIssue.valueChanges
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        tap(() => {
+          this.fetchIssues$.next({ data: 'search' });
+          this.isLoading$.next(true);
+        })
+      )
+      .subscribe();
+    this.userInfo$ = this.loginService.loggedInUserInfo$.pipe(
+      tap(({ permissions = [] }) => this.prepareMenuActions(permissions))
+    );
+    this.displayIssues();
+    this.configOptions.allColumns = this.columns;
+  }
+
+  displayIssues(): void {
+    const issuesOnLoadSearch$ = this.fetchIssues$.pipe(
+      filter(({ data }) => data === 'load' || data === 'search'),
+      switchMap(({ data }) => {
+        this.skip = 0;
+        this.nextToken = '';
+        this.fetchType = data;
+        return this.getIssuesList();
+      })
+    );
+
+    const onScrollIssues$ = this.fetchIssues$.pipe(
+      filter(({ data }) => data !== 'load' && data !== 'search'),
+      switchMap(({ data }) => {
+        if (data === 'infiniteScroll') {
+          this.fetchType = 'infiniteScroll';
+          return this.getIssuesList();
+        } else {
+          return of([]);
+        }
+      })
+    );
+
+    this.initial = {
+      columns: this.columns,
+      data: []
+    };
+    this.issues$ = combineLatest([
+      issuesOnLoadSearch$,
+      onScrollIssues$,
+      this.users$
+    ]).pipe(
+      map(([rows, scrollData]) => {
+        if (this.skip === 0) {
+          this.configOptions = {
+            ...this.configOptions,
+            tableHeight: 'calc(100vh - 390px)'
+          };
+          this.initial.data = rows;
+        } else {
+          this.initial.data = this.initial.data.concat(scrollData);
+        }
+        const issues = this.initial.data?.map((issue) => {
+          if (issue.assignedTo !== null) {
+            const assignee = issue.assignedTo.split(',');
+            const firstAssignee = assignee[0]
+              ? this.userService.getUserFullName(assignee[0])
+              : '';
+            const formattedAssignee =
+              assignee?.length === 1
+                ? firstAssignee
+                : `${firstAssignee} + ${assignee.length - 1} more`;
+            issue = { ...issue, assignedTo: formattedAssignee };
+            return issue;
+          }
+        });
+        this.skip = issues.length;
+        this.dataSource = new MatTableDataSource(issues);
+        return issues;
+      })
+    );
+  }
+
+  getIssuesList() {
+    const obj = {
+      next: this.nextToken,
+      limit: this.limit,
+      searchKey: this.searchIssue.value,
+      type: 'issue'
+    };
+
+    return this.roundPlanObservationsService.getObservations$(obj).pipe(
+      mergeMap(({ rows, next, count }) => {
+        this.nextToken = next;
+        this.isLoading$.next(false);
+        this.issuesCount$ = of(count);
+        return of(rows as any[]);
+      }),
+      catchError(() => {
+        this.isLoading$.next(false);
+        return of([]);
+      })
+    );
+  }
+
+  handleTableEvent = (event): void => {
+    this.fetchIssues$.next(event);
+  };
+
+  cellClickActionHandler = (event: CellClickActionEvent): void => {
+    const { columnId, row } = event;
+    switch (columnId) {
+      default:
+        this.openModal(row);
+    }
+  };
+
+  prepareMenuActions(permissions: Permission[]): void {
+    const menuActions = [];
+
+    if (
+      this.loginService.checkUserHasPermission(
+        permissions,
+        perms.viewORObservations
+      )
+    ) {
+      menuActions.push({
+        title: 'Show Details',
+        action: 'showDetails'
+      });
+    }
+
+    this.configOptions.rowLevelActions.menuActions = menuActions;
+    this.configOptions.displayActionsColumn = menuActions.length ? true : false;
+    this.configOptions = { ...this.configOptions };
+  }
+
+  openModal(row: GetFormList): void {
+    if (this.isModalOpened) {
+      return;
+    }
+    this.isModalOpened = true;
+    const dialogRef = this.dialog.open(IssuesActionsViewComponent, {
+      data: { ...row, users$: this.users$ },
+      maxWidth: '100vw',
+      maxHeight: '100vh',
+      height: '100%',
+      width: '100%',
+      panelClass: 'full-screen-modal'
+    });
+
+    dialogRef.afterClosed().subscribe((resp) => {
+      this.isModalOpened = false;
+      if (resp && Object.keys(resp).length) {
+        const { id, status, priority, dueDate, assignedTo } = resp.data;
+        this.initial.data = this.dataSource.data.map((data) => {
+          if (data.id === id) {
+            return {
+              ...data,
+              status,
+              priority,
+              dueDate: dueDate ? format(new Date(dueDate), 'dd MMM, yyyy') : '',
+              assignedTo
+            };
+          }
+          return data;
+        });
+        this.dataSource = new MatTableDataSource(this.initial.data);
+        this.cdrf.detectChanges();
+      }
+    });
+  }
+
+  rowLevelActionHandler = (event: RowLevelActionEvent) => {
+    const { action, data } = event;
+    switch (action) {
+      case 'showDetails':
+        this.openModal(data);
+        break;
+      default:
+    }
+  };
+}
