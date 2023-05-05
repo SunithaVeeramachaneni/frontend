e914dce0939cf53bcab57eab9b4d19bee4547d1b
Abhishek Satyanarayanan <abhishek.satyanarayanan@innovapptive.com>
2023-05-05 13:23:00 +0530

    Hot fix

--- src/app/components/master-configurations/assets/services/assets.service.ts
+++ src/app/components/master-configurations/assets/services/assets.service.ts
@@ -40,7 +40,6 @@ export class AssetsService {
 
   fetchAllAssets$ = (plantsID = null, info: ErrorInfo = {} as ErrorInfo) => {
     const params: URLSearchParams = new URLSearchParams();
-    params.set('limit', this.MAX_FETCH_LIMIT);
     if (plantsID) {
       const filter = {
         plantsID: {
@@ -49,7 +48,6 @@ export class AssetsService {
       };
       params.set('filter', JSON.stringify(filter));
     }
-    params.set('next', '');
     return this._appService._getResp(
       environment.masterConfigApiUrl,
       'asset/list?' + params.toString(),
