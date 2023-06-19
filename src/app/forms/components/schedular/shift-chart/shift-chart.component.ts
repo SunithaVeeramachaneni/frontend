2b0d1315b0ee1a3e29231aa6e1e1bc987b0c8612
sahil-mendhe <sahil.mendhe@innovapptive.com>
2023-06-19 13:44:13 +0530

    shift dropdown

--- src/app/forms/components/schedular/shift-chart/shift-chart.component.ts
+++ src/app/forms/components/schedular/shift-chart/shift-chart.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
+import { ShiftService } from 'src/app/components/master-configurations/shifts/services/shift.service';
 @Component({
   selector: 'app-shift-chart',
   templateUrl: './shift-chart.component.html',
   styleUrls: ['./shift-chart.component.scss']
 })
 export class ShiftChartComponent implements OnInit {
-  constructor() {}
+  shiftsInformation = [];
+  allShiftsData = [];
+  shifts = new FormControl('');
+  shiftsList: string[] = ['Shift A', 'Shift B', 'Shift C'];
+  constructor(private shiftService: ShiftService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getAllShiftsData();
+  }
+
+  getAllShiftsData() {
+    this.shiftService.fetchAllShifts$().subscribe((shifts) => {
+      this.allShiftsData = shifts.items || [];
+      this.shiftsInformation = this.allShiftsData;
+    });
+    console.log(this.allShiftsData, this.shiftsInformation);
+  }
 }
