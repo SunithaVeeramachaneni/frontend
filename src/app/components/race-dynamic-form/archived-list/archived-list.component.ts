db8ce722f0af86a32c35f19a236109f2b35e92fc
Chinmay Gumgaonkar <chinmay.gumgaonkar@innovapptive.com>
2023-07-06 18:00:29 +0530

    CWP-2991:archieved delete fixed

--- src/app/components/race-dynamic-form/archived-list/archived-list.component.ts
+++ src/app/components/race-dynamic-form/archived-list/archived-list.component.ts
@@ -481,10 +481,10 @@ export class ArchivedListComponent implements OnInit, OnDestroy {
     return this.raceDynamicFormService.updateForm$({
       formMetadata: {
         id: form?.id,
-        isArchived: false,
         name: form?.name,
         description: form?.description,
-        isArchivedAt: ''
+        isArchivedAt: '',
+        isDeleted: true
       },
       // eslint-disable-next-line no-underscore-dangle
       formListDynamoDBVersion: form._version
