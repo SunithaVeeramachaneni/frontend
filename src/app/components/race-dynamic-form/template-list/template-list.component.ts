8caff4d36d490b98f9753c00bdbd663e07c6faca
divyanshu.pandey <divyanshu.pandey@innovapptive.com>
2023-08-02 15:06:39 +0530

    Minor Change

--- src/app/components/race-dynamic-form/template-list/template-list.component.ts
+++ src/app/components/race-dynamic-form/template-list/template-list.component.ts
@@ -223,7 +223,7 @@ export class TemplateListComponent implements OnInit, OnDestroy {
     );
     this.searchTemplates = new FormControl('');
     this.headerService.setHeaderTitle(
-      this.translateService.instant('Templates')
+      this.translateService.instant('templates')
     );
 
     this.usersService.getUsersInfo$().subscribe(() => {
