c26819792c8d2e6dcbc12deca0e48e6f16ba5794
kavyakoka <kavya.koka@innovapptive.com>
2023-08-08 16:29:17 +0530

    removed all hex code from scss files and moved to colors.scss file

--- src/app/shared/components/custom-pagination-controls/custom-pagination-controls.component.ts
+++ src/app/shared/components/custom-pagination-controls/custom-pagination-controls.component.ts
@@ -1,18 +1,19 @@
 import { Component, Input } from '@angular/core';
-import { PaginationControlsComponent, PaginationControlsDirective } from 'ngx-pagination';
+import {
+  PaginationControlsComponent,
+  PaginationControlsDirective
+} from 'ngx-pagination';
 
 @Component({
   selector: 'app-custom-pagination-controls',
   templateUrl: './custom-pagination-controls.component.html',
-  styleUrls: ['./custom-pagination-controls.component.css']
+  styleUrls: ['./custom-pagination-controls.component.scss']
 })
 export class CustomPaginationControlsComponent extends PaginationControlsComponent {
-
   // tslint:disable-next-line: no-input-rename
   @Input('paginationData') p: PaginationControlsDirective;
 
   constructor() {
     super();
   }
-
 }
