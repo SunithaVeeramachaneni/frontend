bf9650c77d462382e46da0a7426e361b25023dd5
Viplav Pandhurnekar <viplav.pandhurnekar@innovapptive.com>
2023-08-11 16:17:04 +0530

    Changes related to Image attachment embedded

--- src/app/forms/components/question/question.component.ts
+++ src/app/forms/components/question/question.component.ts
@@ -162,6 +162,7 @@ export class QuestionComponent implements OnInit, OnDestroy {
   fieldTypes: any = [this.fieldType];
   formMetadata: FormMetadata;
   moduleName: string;
+  showAskQuestionFeatures = true;
 
   get rangeDisplayText() {
     return this._rangeDisplayText;
@@ -322,6 +323,7 @@ export class QuestionComponent implements OnInit, OnDestroy {
           if (!isEqual(prev, curr)) {
             const { value: prevValue } = prev;
             const { value: currValue } = curr;
+            this.checkAskQuestionFeatures();
             if (
               current.fieldType === 'INST' &&
               prevValue !== undefined &&
@@ -395,6 +397,7 @@ export class QuestionComponent implements OnInit, OnDestroy {
     this.questionForm.patchValue(this.question, {
       emitEvent: false
     });
+    this.checkAskQuestionFeatures();
     this.rangeDisplayText = '';
   }
 
@@ -457,6 +460,27 @@ export class QuestionComponent implements OnInit, OnDestroy {
       type: 'delete',
       questionId: this.questionId
     });
+  }
+
+  checkAskQuestionFeatures() {
+    const fieldType = this.questionForm.get('fieldType').value;
+    if (this.isAskQuestion) {
+      switch (fieldType) {
+        case 'SF':
+        case 'CB':
+        case 'SGF':
+        case 'ATT':
+        case 'GAL':
+        case 'DFR':
+        case 'VI':
+          this.showAskQuestionFeatures = false;
+          break;
+        default:
+          this.showAskQuestionFeatures = true;
+      }
+    } else {
+      this.showAskQuestionFeatures = true;
+    }
   }
 
   selectFieldTypeEventHandler(fieldType) {
@@ -565,14 +589,9 @@ export class QuestionComponent implements OnInit, OnDestroy {
     this.formService
       .uploadToS3$(`${this.moduleName}/${this.formMetadata?.id}`, files[0])
       .subscribe(async (data) => {
-        const { base64Response: base64 } =
-          await this.base64Service.getBase64ImageFromSourceUrl(
-            data.message.objectURL
-          );
         const value = {
           name: file.name,
           size: file.size,
-          base64: base64.split(',')[1],
           objectKey: data.message.objectKey,
           objectURL: data.message.objectURL
         };
