333118f7053c35bad2fa906c6efada1b654882e4
Chinmay Gumgaonkar <chinmay.gumgaonkar@innovapptive.com>
2023-09-06 16:20:08 +0530

    moved to development

--- src/app/forms/services/observations.service.ts
+++ src/app/forms/services/observations.service.ts
@@ -487,6 +487,9 @@ export class ObservationsService {
         case 'location':
           item.items = data?.location ?? [];
           break;
+        case 'asset':
+          item.items = data?.asset ?? [];
+          break;
         case 'plant':
           item.items = data?.plant ?? [];
           break;
@@ -537,7 +540,16 @@ export class ObservationsService {
     });
     return allColumns;
   }
-
+  createLocationAssetDescription(data) {
+    let desc = '';
+    if (data.SWERK) {
+      desc = `Location ID: ${data.SWERK}`;
+    }
+    if (data.ANLNR && data.ANLNR !== placeHolder && data.ANLNR !== '--') {
+      desc = ` Asset ID: ${data.ANLNR}`;
+    }
+    return desc;
+  }
   private formateGetObservationResponse(resp, type) {
     if (resp?.filters?.assignedTo?.length > 0) {
       resp.filters.assignedTo = resp.filters.assignedTo
@@ -578,12 +590,7 @@ export class ObservationsService {
         location,
         asset,
         locationAsset: location !== placeHolder ? location : asset,
-        locationAssetDescription:
-          location !== placeHolder
-            ? `Location ID: ${item.SWERK}`
-            : asset !== placeHolder
-            ? `Asset ID: ${item.ANLNR}`
-            : '',
+        locationAssetDescription: this.createLocationAssetDescription(item),
         priority: item.PRIORITY,
         status: item?.STATUS ?? '',
         statusDisplay: this.prepareStatus(item?.STATUS),
