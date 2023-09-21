c53b2c13f952a4b384eec6e53584861ae345314b
Kiran Kumar Palani <kiran.palani@innovapptive.com>
2023-09-21 13:44:45 +0530

    By dates comparision fix

--- src/app/components/operator-rounds/scheduler-modal/scheduler-modal.component.ts
+++ src/app/components/operator-rounds/scheduler-modal/scheduler-modal.component.ts
@@ -13,7 +13,11 @@ import { Step } from 'src/app/interfaces/stepper';
 import { AlertModalComponent } from './alert-modal/alert-modal.component';
 import { OperatorRoundsService } from '../services/operator-rounds.service';
 import { localToTimezoneDate } from 'src/app/shared/utils/timezoneDate';
-import { dateFormat3, dateFormat4 } from 'src/app/app.constants';
+import {
+  dateFormat3,
+  dateFormat4,
+  dateTimeFormat3
+} from 'src/app/app.constants';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { RoundPlanScheduleConfiguration } from 'src/app/interfaces';
@@ -83,7 +87,16 @@ export class SchedulerModalComponent implements OnInit {
           return acc;
         }, {}),
         scheduleType: headerScheduleType,
-        scheduleByDates: headerScheduleByDates,
+        scheduleByDates: headerScheduleByDates.map((scheduleByDate) => ({
+          ...scheduleByDate,
+          date: new Date(
+            localToTimezoneDate(
+              new Date(scheduleByDate.date),
+              this.plantTimezoneMap[this.data.roundPlanDetail?.plantId],
+              dateTimeFormat3
+            )
+          )
+        })),
         endDate: localToTimezoneDate(
           new Date(headerEndDate),
           this.data.plantTimezoneMap[this.data.roundPlanDetail?.plantId],
