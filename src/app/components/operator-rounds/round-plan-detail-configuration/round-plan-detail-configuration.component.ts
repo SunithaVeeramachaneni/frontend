21b2b9b9d9bb398510825baadd3a121c62e17be3
Amit Malviya <amit.malviya@innovapptive.com>
2023-09-27 11:18:42 +0000

    CWP-5866: Ability to collapse & expand all the sections of Forms & Rounds

--- src/app/components/operator-rounds/round-plan-detail-configuration/round-plan-detail-configuration.component.ts
+++ src/app/components/operator-rounds/round-plan-detail-configuration/round-plan-detail-configuration.component.ts
@@ -122,6 +122,7 @@ export class RoundPlanDetailConfigurationComponent
   createOrEditForm: boolean;
   isPreviewActive = false;
   readonly formConfigurationStatus = formConfigurationStatus;
+  collapseAllSections: FormControl = new FormControl(false);
   authoredRoundPlanDetailSubscription: Subscription;
   private destroy$ = new Subject();
 
@@ -314,6 +315,8 @@ export class RoundPlanDetailConfigurationComponent
           subFormsObj[key] = formDetails[key];
         });
 
+        this.setCollapseAllSectionsState(subFormsObj, this.selectedNode?.id);
+
         this.formListVersion = formListDynamoDBVersion;
         this.formStatus = formStatus;
         this.formDetailPublishStatus = formDetailPublishStatus;
@@ -498,6 +501,15 @@ export class RoundPlanDetailConfigurationComponent
         }
       })
     );
+
+    this.collapseAllSections.valueChanges.subscribe((isCollapse) => {
+      this.store.dispatch(
+        BuilderConfigurationActions.updateAllSectionState({
+          isCollapse,
+          subFormId: this.selectedNode.id
+        })
+      );
+    });
   }
 
   retrieveDetails() {
@@ -682,4 +694,46 @@ export class RoundPlanDetailConfigurationComponent
   goToPDFBuilderConfiguration = () => {
     this.gotoNextStep.emit();
   };
+
+  private setCollapseAllSectionsState(subFormsObj = {}, selectedNodeId = null) {
+    let pages = [];
+    if (selectedNodeId) {
+      let key = 'pages';
+      const subFormId = selectedNodeId;
+      if (subFormId) {
+        key = `${key}_${subFormId}`;
+      }
+      pages = subFormsObj[key] || [];
+    }
+    if (
+      !subFormsObj ||
+      Object.values(subFormsObj)?.flat()?.length === 0 ||
+      pages?.length === 0
+    ) {
+      this.collapseAllSections.setValue(false, {
+        emitEvent: false
+      });
+      return;
+    }
+    let allSections = 0;
+    let closedSections = 0;
+    if (subFormsObj) {
+      if (pages?.length > 0) {
+        pages.forEach((page) => {
+          allSections += page?.sections?.length;
+          closedSections +=
+            page?.sections?.filter((section) => !section?.isOpen)?.length || 0;
+        });
+      } else {
+        this.collapseAllSections.setValue(false, {
+          emitEvent: false
+        });
+        return;
+      }
+    }
+    const allCollapse: boolean = closedSections === allSections ? true : false;
+    this.collapseAllSections.setValue(allCollapse, {
+      emitEvent: false
+    });
+  }
 }
