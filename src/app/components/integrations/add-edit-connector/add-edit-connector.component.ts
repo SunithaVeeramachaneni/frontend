bb43fd0f9b8c4904607eae90261b874baa1b179c
Priyanka Singh <priyanka.singh@innovapptive.com>
2023-11-01 12:46:32 +0530

    Password view enabled only for Super Admin.

--- src/app/components/integrations/add-edit-connector/add-edit-connector.component.ts
+++ src/app/components/integrations/add-edit-connector/add-edit-connector.component.ts
@@ -17,6 +17,7 @@ import { ErrorInfo } from 'src/app/interfaces';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { permissions } from 'src/app/app.constants';
 import { ToastService } from 'src/app/shared/toast';
+import { LoginService } from '../../login/services/login.service';
 
 @Component({
   selector: 'app-add-edit-connector',
@@ -78,9 +79,11 @@ export class AddEditConnectorComponent implements OnInit {
     description: new FormControl(''),
     icon: new FormControl('')
   });
+  superAdminEnable = false;
 
   constructor(
     public dialogRef: MatDialogRef<AddEditConnectorComponent>,
+    private loginService: LoginService,
     private fb: FormBuilder,
     private cdrf: ChangeDetectorRef,
     @Inject(MAT_DIALOG_DATA)
@@ -90,6 +93,10 @@ export class AddEditConnectorComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    const userInfo = this.loginService.getLoggedInUserInfo();
+    if (userInfo.roles[0].name === 'Super Admin') {
+      this.superAdminEnable = true;
+    }
     if (this.data.mode === 'edit') {
       const connector = this.data?.connector;
       this.connectionForm.patchValue({
